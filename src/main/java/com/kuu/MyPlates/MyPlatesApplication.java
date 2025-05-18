6b529c0f61646fd6d2ae739b436f2073c89cf20e
kuu18 <kdai18.kuu@gmail.com>
2025-05-18 15:28:47 +0900

    initial commmit

---
+++ src/main/java/com/kuu/MyPlates/MyPlatesApplication.java
@@ -0,0 +1,13 @@
+package com.kuu.MyPlates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPlatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuu/MyPlates/MyPlatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuu.MyPlates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlatesApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
